6a6d305b9a2063e493a03f16c0756af1d5317466
Goutham <gouthamcold@gmail.com>
2018-04-02 09:25:37 -0400

    Added db configuration file and wrote connection code for db in app.js

--- app.js
+++ app.js
@@ -6,18 +6,30 @@ var passport = require('passport');
 var mongoose = require('mongoose');
 //requiring contoller
 var apiRoutes = require('./controllers/apiController');
+//dbConnection
+var conn = require('./config/connection');
+////////////////////////////////////////////////////////////////////////////End of requires
 
 //app variable in initialized
 var app = express();
 
+///////////////////////////////////////////////////////////////////route configuration
 //Initialzing the user routes for the application
 apiRoutes(app);
 
 
+/////////////////////////////////////////////DB Connection and initial check
+//Configuring DB connection
+mongoose.connect(conn.dbConnection());
+//Checking the db connectivity
+mongoose.connection.on('connected', ()=>{
+    console.log('Connection made to db');
+})
 
 //The server uses the middle ware cors in order to get req from angular to process it in node
 app.use(cors())
-//The server is listening to the port 300
+
+//The server is listening to the port 3000
 app.listen(3000, () => {
     console.log("Server has been started");
 });
---
+++ config/connection.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbConnection : function(){
+        return "mongodb://localhost:27017/meanAuth";
+    }
+}
