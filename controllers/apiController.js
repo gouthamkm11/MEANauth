686552ddec453783be9ed41a29eeff705f7a5f0e
Goutham <gouthamcold@gmail.com>
2018-04-02 19:50:53 -0400

    added methods in /users/register route

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,21 +1,39 @@
+var users = require('../model/users'); //Importing the usersmodel from model folder
+var passport = require('passport');
+var jwt = require('jsonwebtoken');
+//Body parser and urlEncodedParser to extract contents from body of http request for a post method
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlEncodedParser({extended:false});
+//////////////////////////////////////////End of imports
+
 module.exports = function(app){
     app.get('/', (req,res)=>{
-        res.send("Server Connected");
+        res.send("Server connected");
     });
 
-    app.get('/users/register', (req,res)=>{
-        res.send("Register Route created")
+    app.post('/users/register', urlEncodedParser, (req,res)=>{
+        let newUser = new users({
+            name: req.body.name,
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        users.addUser(newUser, (err, user)=>{
+            if(err){
+                res.json({success:false, message:'User not registered'})
+            }
+            else{
+                res.json({success:true, message:'User registered'})
+            }
+        });
     });
 
-    app.get('/users/authenticate', (req,res)=>{
+    app.post('/users/authenticate', urlEncodedParser, (req,res)=>{
         res.send("Authenticate Route created")
     });
 
     app.get('/users/profile', (req,res)=>{
         res.send("Profile Route created")
-    });
-
-    app.get('/users/Validate', (req,res)=>{
-        res.send("Validate Route created")
     });
 }
